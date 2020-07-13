c9538f1263354a6b17070e37f4849ce8f6e926e0
André <ruland@outlook.de>
2020-07-13 15:03:03 +0200

    noSQL mit LiteDB

---
+++ HalloLiteDB/HalloLiteDB/Program.cs
@@ -0,0 +1,38 @@
+﻿using LiteDB;
+using System;
+
+namespace HalloLiteDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var db = new LiteDatabase(@"C:\db\lll.db"))
+            {
+                var col = db.GetCollection<Auto>("Autos");
+
+                foreach (var item in col.Query().ToList())
+                {
+                    //Console.WriteLine($"Hersteller: {item.Hersteller}");
+                    Console.WriteLine($"Farbe: {item.Farbe}");
+                }
+
+                //col.Insert(new Auto() { Hersteller = "Baudi",Farbe="blau" });
+                //col.Insert(new Auto() { Farbe="rot" });
+
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Auto
+    {
+        public int Id { get; set; }
+
+        public string Farbe { get; set; }
+    }
+}

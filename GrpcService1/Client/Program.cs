fd09f6ef2c801c3fc2832fc1d5aa98d1a59e661a
André <ruland@outlook.de>
2020-07-14 14:14:14 +0200

    gRPC

---
+++ GrpcService1/Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new GrpcService1.Greeter.GreeterClient(channel);
+
+            var res = client.SayHello(new HelloRequest() { Name = "Fred" });
+            Console.WriteLine(res.Message);
+        }
+    }
+}

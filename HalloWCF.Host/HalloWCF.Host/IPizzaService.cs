e40435c9f508ac1ea9ea173cec96e9a203b938b1
André <ruland@outlook.de>
2020-07-14 09:59:22 +0200

    WCF ist cool, aber tod

---
+++ HalloWCF.Host/HalloWCF.Host/IPizzaService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HalloWCF.Host
+{
+    [ServiceContract]
+    public interface IPizzaService
+    {
+        [OperationContract]
+        IEnumerable<Pizza> GetPizzaListe();
+    }
+
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Preis { get; set; }
+        public string PreisUnit { get; set; } = "€";
+    }
+
+}
---
+++ HalloWCF.Host/HalloWCF.Host/PizzaService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HalloWCF.Host
+{
+    public class PizzaService : IPizzaService
+    {
+        public IEnumerable<Pizza> GetPizzaListe()
+        {
+            yield return new Pizza()
+            { Id = 1, Name = "Salami", Preis = 12.6m };
+
+            yield return new Pizza()
+            { Id = 2, Name = "Schinken", Preis = 13.6m };
+
+            yield return new Pizza()
+            { Id = 3, Name = "Hawai", Preis = 10m };
+        }
+    }
+}

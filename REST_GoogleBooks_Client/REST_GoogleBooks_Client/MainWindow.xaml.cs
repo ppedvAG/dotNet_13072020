38a3a5d79326c5f0330908b261dacacc46eb17f2
André <ruland@outlook.de>
2020-07-14 10:27:07 +0200

    REST BookClient WPF

---
+++ REST_GoogleBooks_Client/REST_GoogleBooks_Client/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Windows;
+
+namespace REST_GoogleBooks_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Suchen(object sender, RoutedEventArgs e)
+        {
+            var url = $"https://www.googleapis.com/books/v1/volumes?q={tb1.Text}";
+
+            var http = new HttpClient();
+            var json = await http.GetStringAsync(url);
+            jsonTb.Text = json;
+        }
+    }
+}
